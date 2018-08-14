59e2d32870f9a1260d999caa4ecd6378893beec4
abhishek-kharb <abhishekkharb@Abhisheks-MacBook-Pro.local>
2018-08-14 13:18:59 +0530

    Resetup example at correct path

---
+++ MycroftExample/MYAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  MYAppDelegate.h
+//  Mycroft
+//
+//  Created by Abhishek Kharb on 14/08/18.
+//  Copyright © 2018 Hike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@end
---
+++ MycroftExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mycroft
+//
+//  Created by Abhishek Kharb on 14/08/18.
+//  Copyright © 2018 Hike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MYAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MYAppDelegate class]));
+    }
+}
